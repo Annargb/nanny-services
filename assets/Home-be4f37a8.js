d0c51f8a4d1c8342c2cdb6a58b2c9ea822aa0cd2
Annargb <130927958+Annargb@users.noreply.github.com>
2024-04-19 00:35:18 +0000

    Deploying to gh-pages from @ Annargb/nanny-services@3f43c7c2e0a57a659f0c09235f2bbf3701be65ed 🚀

--- assets/Home-be4f37a8.js
+++ assets/Home-be4f37a8.js
@@ -1,4 +1,4 @@
-import{u as r,L as i,j as o,i as t}from"./index-a0c67e7e.js";import{h as n}from"./home1x-0c9ce9af.js";const s="/nanny-services/assets/home2x-1ca38526.png",p=r.div`
+import{u as r,L as n,j as o,i}from"./index-fef374f5.js";import{h}from"./home1x-0c9ce9af.js";const p="/nanny-services/assets/home2x-1ca38526.png",a=r.div`
   display: flex;
   width: 1376px;
   height: 736px;
@@ -8,23 +8,39 @@ import{u as r,L as i,j as o,i as t}from"./index-a0c67e7e.js";import{h as n}from"
   width: 677px;
   height: 736px;
   padding: 251px 64px 0 96px;
-`,h=r.h1`
+`,x=r.h1`
   font-weight: 500;
   font-size: 70px;
   line-height: 1;
   letter-spacing: -0.03em;
   color: ${e=>e.theme.colors.white};
   margin-bottom: 28px;
-`,x=r.p`
+`,l=r.p`
   font-weight: 400;
   font-size: 28px;
   line-height: 1.07;
   letter-spacing: -0.02em;
   color: ${e=>e.theme.colors.white};
   margin-bottom: 64px;
-`,a=r(i)`
+`,t=r.svg`
+  position: absolute;
+  top: 22px;
+  right: 45px;
+  width: 15px;
+  height: 17px;
+
+  transition: transform 250ms ${e=>e.theme.transition};
+`,s=r(t)`
+  width: 20px;
+  height: 16px;
+  right: 40px;
+  transform: translateX(calc(50% + 50px));
+`,d=r(n)`
+  position: relative;
   display: inline-block;
+  width: 230px;
   padding: 18px 50px;
+  overflow: hidden;
   background-color: transparent;
   border: 1px solid;
   border-color: ${e=>e.theme.colors.borderColor};
@@ -34,11 +50,15 @@ import{u as r,L as i,j as o,i as t}from"./index-a0c67e7e.js";import{h as n}from"
   line-height: 1.2;
   letter-spacing: -0.01em;
   color: ${e=>e.theme.colors.white};
-`,d=r.svg`
-  width: 15px;
-  height: 17px;
-  margin-left: 18px;
-`,l=r.div`
+
+  &:hover ${t}, &:focus ${t} {
+    transform: translateY(-55px);
+  }
+
+  &:hover ${s}, &:focus ${s} {
+    transform: translateX(calc(-50% + 10px));
+  }
+`,g=r.div`
   position: relative;
   max-width: 699px;
   width: 100%;
@@ -48,11 +68,11 @@ import{u as r,L as i,j as o,i as t}from"./index-a0c67e7e.js";import{h as n}from"
       ${e=>e.theme.colors.gradientColor},
       ${e=>e.theme.colors.gradientColor}
     ),
-    image-set(url(${n}) 1x, url(${s}) 2x);
+    image-set(url(${h}) 1x, url(${p}) 2x);
   background-size: cover;
   border-top-right-radius: 30px;
   border-bottom-right-radius: 30px;
-`,g=r.div`
+`,m=r.div`
   display: flex;
   gap: 16px;
   position: absolute;
@@ -63,7 +83,7 @@ import{u as r,L as i,j as o,i as t}from"./index-a0c67e7e.js";import{h as n}from"
   padding: 32px;
   border-radius: 20px;
   background-color: ${e=>e.theme.colors.white};
-`,m=r.div`
+`,f=r.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -71,16 +91,16 @@ import{u as r,L as i,j as o,i as t}from"./index-a0c67e7e.js";import{h as n}from"
   width: 54px;
   height: 54px;
   background-color: ${e=>e.theme.colors.green};
-`,f=r.svg`
+`,u=r.svg`
   width: 20px;
   height: 16px;
-`,u=r.p`
+`,b=r.p`
   margin-bottom: 6px;
   font-weight: 400;
   font-size: 16px;
   color: ${e=>e.theme.colors.textGrayColor};
-`,b=r.p`
+`,w=r.p`
   font-weight: 700;
   font-size: 24px;
   color: ${e=>e.theme.colors.black};
-`,k=()=>o.jsxs(p,{children:[o.jsxs(c,{children:[o.jsx(h,{children:"Make Life Easier for the Family:"}),o.jsx(x,{children:"Find Babysitters Online for All Occasions"}),o.jsxs(a,{to:"/nannies",children:["Get started",o.jsx(d,{children:o.jsx("use",{href:`${t}#arrow-up`})})]})]}),o.jsx(l,{children:o.jsxs(g,{children:[o.jsx(m,{children:o.jsx(f,{children:o.jsx("use",{href:`${t}#check`})})}),o.jsxs("div",{children:[o.jsx(u,{children:"Experienced nannies"}),o.jsx(b,{children:"15,000"})]})]})})]});export{k as default};
+`,k=()=>o.jsxs(a,{children:[o.jsxs(c,{children:[o.jsx(x,{children:"Make Life Easier for the Family:"}),o.jsx(l,{children:"Find Babysitters Online for All Occasions"}),o.jsxs(d,{to:"/nannies",children:["Get started",o.jsx(t,{children:o.jsx("use",{href:`${i}#arrow-up`})}),o.jsx(s,{children:o.jsx("use",{href:`${i}#arrow-right`})})]})]}),o.jsx(g,{children:o.jsxs(m,{children:[o.jsx(f,{children:o.jsx(u,{children:o.jsx("use",{href:`${i}#check`})})}),o.jsxs("div",{children:[o.jsx(b,{children:"Experienced nannies"}),o.jsx(w,{children:"15,000"})]})]})})]});export{k as default};
