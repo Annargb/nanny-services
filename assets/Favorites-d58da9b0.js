16e83b5555fde95da8ad2fc753bf86213f2a7060
Annargb <130927958+Annargb@users.noreply.github.com>
2024-04-22 10:08:28 +0000

    Deploying to gh-pages from @ Annargb/nanny-services@2513451d540f846c49c7b790679401a85fbeee17 🚀

---
+++ assets/Favorites-d58da9b0.js
@@ -0,0 +1,1 @@
+import{d as e,r,l as i,j as s,i as a}from"./index-21d1c137.js";const h=()=>{const t=e();return r.useEffect(()=>{t(i())},[t]),s.jsxs("div",{children:[s.jsx("h1",{children:"Favorites"}),s.jsx("svg",{children:s.jsx("use",{href:`${a}#icon-heart`,style:{width:"30px",height:"30px"}})})]})};export{h as default};
