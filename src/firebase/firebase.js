1648b06beb9d168d24aa1c2697630e81e4218d69
Annargb <annasamoylenko2015@gmail.com>
2024-05-04 13:36:04 +0200

    update firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,18 +13,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAnkp7Cdlmpf9PjBnpYwp9-0ze0F47L7hE",
-//   authDomain: "nany-services-9a09d.firebaseapp.com",
-//   databaseURL:
-//     "https://nany-services-9a09d-default-rtdb.europe-west1.firebasedatabase.app/",
-//   projectId: "nany-services-9a09d",
-//   storageBucket: "nany-services-9a09d.appspot.com",
-//   messagingSenderId: "734209943459",
-//   appId: "1:734209943459:web:bbf6c4586c1423684a5c16",
-//   measurementId: "G-CPK9PRQMWC",
-// };
-
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
